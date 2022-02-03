fc4324c89d4ab85d980e961bc0f74ff988ece88a
Vishnupriya <vishnupriya@Vishnupriyas-MacBook-Air.local>
2022-02-03 10:39:49 +0530

    Fixed Bugs, and implemented days calculation

--- Destination/script.js
+++ Destination/script.js
@@ -27,65 +27,106 @@ const startInput = document.querySelector(".start");
 const endInput = document.querySelector(".end");
 const dateInput = document.querySelector(".date");
 const result = document.querySelector(".result-div");
+const dateNow = new Date().toISOString().split("T").shift();
+dateInput.value = dateNow;
 let tempRouteArr = [],
   tempDistArr = [],
   routeArr = [],
   distArr = [],
-  count = 0;
+  count = 0,
+  newDate;
+
 places.forEach((item) => {
   startInput.innerHTML += `<option value="${item}">${item}</option>`;
   endInput.innerHTML += `<option value="${item}">${item}</option>`;
 });
 
 document.querySelector(".go-btn").addEventListener("click", () => {
-  getResults();
+  const startValue = startInput.value;
+  const endValue = endInput.value;
+  getResults(startValue, endValue);
 });
+
+function getResults(startValue, endValue) {
+  checkRoute(startValue, endValue);
+  optimizeRoute(routeArr, distArr, endValue);
+  displayResults(routeArr, distArr);
+  console.log(routeArr);
+  console.log(distArr);
+}
 function reset() {
   location.reload();
 }
-
-function getResults() {
-  const startValue = startInput.value;
-  const endValue = endInput.value;
-
-  checkRoute(startValue, endValue);
-  displayResults(routeArr, distArr);
-}
-
 function checkRoute(startValue, endValue) {
+  if (startValue === endValue) {
+    tempRouteArr.push(`${startValue}`);
+    routeArr.push([...tempRouteArr.reverse()]);
+    distArr.push(tempDistArr.reduce((a, b) => a + b, 0));
+    tempRouteArr.length = 0;
+    tempDistArr.length = 0;
+    count = 0;
+    return;
+  }
+
   for (let i = 0; i < data.length; i++) {
-    if (startValue === endValue) {
-      tempRouteArr.push(`${startValue}`);
-      routeArr[count] = [...tempRouteArr.reverse()];
-      distArr[count] = tempDistArr.reduce((a, b) => a + b, 0);
-      count++;
-      tempRouteArr.length = 0;
-      tempDistArr.length = 0;
-      return;
-    }
     if (endValue === data[i].end) {
+      console.log(data[i].end + "<-" + data[i].start);
       tempRouteArr.push(`${data[i].end}`);
       tempDistArr.push(data[i].days);
       checkRoute(startValue, data[i].start);
     }
   }
 }
-function displayResults(routeArr, distArr) {
-  let index = distArr.indexOf(Math.min(...distArr));
-  let dateValue = new Date(dateInput.value);
-  let newDate = new Date(
-    dateValue.setDate(dateValue.getDate() + Math.min(...distArr))
-  );
-
-  var someFormattedDate = newDate.toLocaleDateString();
-  result.innerHTML += `So the best route is..`;
+function optimizeRoute(routeArr, distArr, endValue) {
   for (let i = 0; i < routeArr.length; i++) {
-    if (i === routeArr.length - 1) {
-      result.innerHTML += ` ${routeArr[index][i]}!`;
-    } else result.innerHTML += ` ${routeArr[index][i]} -> `;
+    if (endValue !== routeArr[i][routeArr[i].length - 1]) {
+      data.forEach((item) => {
+        if (
+          endValue === item.end &&
+          item.start === routeArr[i][routeArr[i].length - 1]
+        ) {
+          routeArr[i].push(item.end);
+          distArr[i] = distArr[i] + item.days;
+        }
+      });
+    } //else console.log(routeArr);
   }
-  result.innerHTML += `  That is going to take ${Math.min(
-    ...distArr
-  )} days. So, You will reach there by ${someFormattedDate}`;
-  result.classList.add("result-class");
+}
+function displayResults(routeArr, distArr) {
+  if (routeArr.length === 0) {
+    result.innerHTML = "Oops!!! No Route Found 😓";
+  } else {
+    result.innerHTML = "";
+    let min = Math.min(...distArr);
+    let formattedDate = dateCalculation(min);
+    let index = distArr.indexOf(min);
+
+    result.innerHTML += `So the best route is.. <br><br>`;
+    for (let i = 0; i < routeArr[index].length; i++) {
+      if (routeArr[index].length - 1 === i) {
+        result.innerHTML += `${routeArr[index][i]}`;
+      } else result.innerHTML += `${routeArr[index][i]} -> `;
+    }
+
+    result.innerHTML += ` <br><br> That is going to take ${min} days. So, Will reach there by ${formattedDate}`;
+    result.classList.add("result-class");
+  }
+
+  function dateCalculation(num) {
+    let dateValue = new Date(dateInput.value);
+    let dayValue = dateValue.getDay() + 1;
+    let numCopy = num;
+    while (numCopy >= 0) {
+      if (dayValue < 7 && dayValue > 1) {
+        newDate = new Date(dateValue.setDate(dateValue.getDate() + 1));
+        dayValue = newDate.getDay() + 1;
+      } else {
+        newDate = new Date(dateValue.setDate(dateValue.getDate() + 2));
+        dayValue = newDate.getDay() + 1;
+      }
+      numCopy--;
+    }
+    var someFormattedDate = newDate.toLocaleDateString();
+    return someFormattedDate;
+  }
 }
